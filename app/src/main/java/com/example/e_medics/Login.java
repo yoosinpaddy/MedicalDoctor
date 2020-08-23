30050729e5b47be6c86a61996167024c9b88b432
yoosinpaddy <yoosinfrancis@gmail.com>
2020-08-23 21:50:46 +0300

    Register Successfull

--- app/src/main/java/com/example/e_medics/Login.java
+++ app/src/main/java/com/example/e_medics/Login.java
@@ -2,6 +2,7 @@ package com.example.e_medics;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -11,6 +12,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
     FirebaseAuth auth;
+    private static final String TAG = "Login";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +26,11 @@ public class Login extends AppCompatActivity {
                 if (task.isSuccessful()) {
                     Login.this.startActivity(new Intent(Login.this.getApplicationContext(), Dashboard.class));
                     return;
+                }else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                    Toast.makeText(Login.this, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show();
                 }
                 Toast.makeText(Login.this.getApplicationContext(), "Error Login Signup", 1).show();
                 Login.this.startActivity(new Intent(Login.this.getApplicationContext(), Signup.class));
